0a87ee9a8caa1c1dd623f32a9e0939ef4e75423f
Aparna Ashok <ashok.a@husky.neu.edu>
2019-03-25 20:05:27 -0400

    input texts added

--- js/route.js
+++ js/route.js
@@ -4,21 +4,35 @@ import 'rxjs/Observable/of';
 
 
 let rxjs = require('rxjs');
-let nextNode = document.querySelector(".add-contact");
-const event$ = rxjs.fromEvent(nextNode, 'click');
-let subscription = event$.subscribe(function (x) {
+let addContactButton = document.querySelector(".add-contact");
+const event$ = rxjs.fromEvent(addContactButton, 'click');
+let subscription1 = event$.subscribe(function (x) {
+    showAddContactView();
+    //createContact();
+});
+
+let submitContactButton = document.querySelector(".submit-button");
+const event1$ = rxjs.fromEvent(submitContactButton, 'click');
+let subscription2 = event1$.subscribe(function () {
     createContact();
 });
 
 let createContact = function createContact(){
+    let firstname = document.querySelector(".firstname").value;
+    let lastname = document.querySelector(".lastname").value;
+    let phoneNumber = document.querySelector(".phone-number").value;
+    let email = document.querySelector(".phone-email").value;
+
     fetch('http://localhost:3000/contacts', {
         mode: "cors",
         headers: { "Content-Type": "application/json"},
         method: 'POST',
         body: JSON.stringify({
-            name: 'Nikola Tesla'
+            firstname: firstname,
+            lastname: lastname,
+            phoneNumber: phoneNumber,
+            email: email
         })
-
     })
             .then(response => console.log("response status", response.status))
             .then(data => console.log("data", JSON.stringify(data)));
@@ -35,6 +49,17 @@ let fetchContacts = function fetchContacts() {
         .then(data => console.log(data))
 }
 
+let showAddContactView = function showAddContactView() {
+    let addContactView = document.querySelector(".contact-form");
+    addContactView.style.display = "block";
+
+}
+
+let hideAddContactView = function hideAddContactView() {
+    let addContactView = document.querySelector(".contact-form");
+    addContactView.style.display = "none";
+
+}
 
 
 // function getAllContacts(oFormElement)
